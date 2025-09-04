6fa09534d5cae5e8190da64754db2df3da6ff1fd
Ian Eckhardt Alvarez <Ian_E_A@yahoo.com>
2025-09-04 09:42:23 -0600

    Commit 3

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,25 +3,51 @@ import java.util.*;
 public class Main {
     static class Usuario{
         public String nombre, correo, password;
-    }
-    public static void crearCuenta(Scanner Scan){
-        Scanner scn = Scan;
-        System.out.println("Por hacer");
-    }
-    public static void logIn(Scanner Scan){
-        System.out.println("##############################");
-        System.out.println("     Ingresar al sistema");
-        System.out.println("##############################");
-        System.out.println("       Crear Cuenta (1)");
-        System.out.println("         Ingresar (2)");
-        System.out.println("##############################");
-        System.out.print("Ingresa un número: ");
-        Scanner scn = Scan;
-        byte a = scn.nextByte();
-        if (a==1){
-            crearCuenta(scn);
+
+        public Usuario(String nombre, String correo, String password) {
+            this.nombre = nombre;
+            this.correo = correo;
+            this.password = password;
         }
-        System.out.println("Por hacer");
+    }
+    public static void crearCuenta(Scanner scn){
+        scn.nextLine();
+        String n, c, p;
+        System.out.print("Ingresa tu nombre: ");
+        n = scn.nextLine();
+        System.out.print("Ingresa tu correo: ");
+        c = scn.nextLine();
+        System.out.print("Ingresa una contraseña para tu cuenta: ");
+        p = scn.nextLine();
+        Usuario newUser = new Usuario(n, c, p);
+        System.out.println(newUser);
+    }
+    public static void logIn(Scanner scn){
+        System.out.println("##############################");
+        System.out.println("|    Ingresar al sistema     |");
+        System.out.println("##############################");
+        System.out.println("|      Crear Cuenta (1)      |");
+        System.out.println("|        Ingresar (2)        |");
+        System.out.println("##############################");
+        boolean x = true;
+        while(x){
+            System.out.print("Ingresa un número: ");
+            try {
+                byte a = scn.nextByte();
+                if (a == 1) {
+                    crearCuenta(scn);
+                    x = false;
+                } else if (a == 2) {
+                    System.out.println("Por hacer");
+                    x = false;
+                } else {
+                    System.out.println("Número inválido, por favor ingresa un número válido (1 o 2)");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Debe ingresar un número válido (1 o 2).");
+                scn.nextLine();
+            }
+        }
 
     }
     public static void main(String[] args) {
