8d02ecc0850cc28ee96dd93441424cf8566dd8b3
Ian Eckhardt Alvarez <Ian_E_A@yahoo.com>
2025-09-03 22:16:37 -0600

    Commit 2

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static class Usuario{
+        public String nombre, correo, password;
+    }
+    public static void crearCuenta(Scanner Scan){
+        Scanner scn = Scan;
+        System.out.println("Por hacer");
+    }
+    public static void logIn(Scanner Scan){
+        System.out.println("##############################");
+        System.out.println("     Ingresar al sistema");
+        System.out.println("##############################");
+        System.out.println("       Crear Cuenta (1)");
+        System.out.println("         Ingresar (2)");
+        System.out.println("##############################");
+        System.out.print("Ingresa un número: ");
+        Scanner scn = Scan;
+        byte a = scn.nextByte();
+        if (a==1){
+            crearCuenta(scn);
         }
+        System.out.println("Por hacer");
+
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        logIn(scn);
     }
 }
