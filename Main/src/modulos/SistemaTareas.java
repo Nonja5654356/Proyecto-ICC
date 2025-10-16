75b789902a93a95741255c611580eba6a23f6025
Ian Eckhardt Alvarez <Ian_E_A@yahoo.com>
2025-10-16 09:09:56 -0600

    Commit 9

--- Main/src/modulos/SistemaTareas.java
+++ Main/src/modulos/SistemaTareas.java
@@ -1,4 +1,4 @@
 package modulos;
-//In this file is where the chores system is on, explained on the README
+//In this file is where the chores system is on, explained on the README.md
 public class SistemaTareas {
 }
