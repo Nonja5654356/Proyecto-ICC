c04a95fc069f72fbfb649e95e9ca5df0a8286861
Ian Eckhardt Alvarez <Ian_E_A@yahoo.com>
2025-10-29 19:31:57 -0600

    Commit 39 (small tweak to main for java 25)

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -142,7 +142,7 @@ public class Main {
         return true;
     }
 
-    public static void main(String[] args) {
+    static void main() {
         boolean x = true;
         Scanner scn = new Scanner(System.in);
         BaseDeDatos baseDeDatos = new BaseDeDatos();
