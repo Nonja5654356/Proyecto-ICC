c5c6e2776faa98f004bf10c690336f7ccd04d7a7
Ian Eckhardt Alvarez <Ian_E_A@yahoo.com>
2025-11-17 10:08:39 -0600

    Commit 53 (password)

--- Main/src/modulos/Usuario.java
+++ Main/src/modulos/Usuario.java
@@ -40,7 +40,7 @@ public abstract class Usuario{
     public void mostrarInfo(){
         System.out.println("Nombre : " + nombre);
         System.out.println("Correo : " + correo);
-        System.out.println("Contraseña : " + "*".repeat(password.length()));
+        System.out.println("Contraseña : " + password);
         System.out.println("Estado de cuenta : " + (alta ? "Dado de alta" : "No dado de alta aún"));
     }
 
